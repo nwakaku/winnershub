cd85317d09b33b362df86d9f9271404e56f031c5
wisdom nwakaku <nwakakucc@gmail.com>
2021-09-23 14:18:18 +0100

    Updates

--- static/js/main.49896dd9.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpwinnerhub=this.webpackJsonpwinnerhub||[]).push([[0],{10:function(n,e,t){"use strict";t.r(e);var r=t(0),c=t.n(r),i=t(3),o=t.n(i),s=(t(8),t(1)),u=function(){return Object(s.jsx)("div",{children:"hello world"})};o.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(u,{})}),document.getElementById("root"))},8:function(n,e,t){}},[[10,1,2]]]);
-//# sourceMappingURL=main.49896dd9.chunk.js.map
